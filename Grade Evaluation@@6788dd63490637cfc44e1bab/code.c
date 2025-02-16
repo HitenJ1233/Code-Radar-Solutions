38f49ff61147a9e0c8fdaebacc7c9a395ad7d980
Hiten Jindal <hitenjindal79@gmail.com>
2025-02-16 11:47:25 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    char ch;
+    scanf("%c",&ch);
+    if (ch==A){
+        printf("Excellent");
+    } else if(ch== B){
+        print("Good");
+    }else if(ch== c){
+        print("Average");
+    } else if(ch== d){
+        print("Below Average");
+    } else if(ch== F){
+        print("Fail");
+    }
+    return 0;
+}
