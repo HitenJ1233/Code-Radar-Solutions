f5a32cd8eef9b9c5e0510155353e191c1a4ac2e9
Hiten Jindal <hitenjindal79@gmail.com>
2025-03-18 18:56:35 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,12 +5,12 @@ int main(){
     scanf("%d %d %c",&a,&b,&c);
     if (c=='+'){
         printf("d",a+b);
-    } if else(c=='-'){ 
+    } else if(c=='-'){ 
         printf("%d",a-b);
-    } if else(c=='-'){ 
-        printf("%d",a-b);
-    }if else(c=='-'){ 
-        printf("%d",a-b);
+    } else if(c=='*'){ 
+        printf("%d",a*b);
+    } else if (c=='/'){ 
+        printf("%d",a/b);
     } 
 return 0;
 }
